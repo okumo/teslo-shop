b8d9c0b9584bc9a5a447f351c8dafa25d9ee421b
Oscar Caballero <44917174+okumo@users.noreply.github.com>
2025-01-23 13:07:22 -0500

    Add files via upload

---
+++ nextauth.d.ts
@@ -0,0 +1,14 @@
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      emailVerified?: string;
+      role: string;
+      image?: string;
+    } & DefaultSession["user"];
+  }
+}
